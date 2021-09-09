9ee972b443e2fd9c936854225c51f41386c577cc
Piotr <p.skolorzynski@gmail.com>
2021-09-09 15:28:05 +0200

    modify App component create function generateId modify form create emebd inputs with useState hooks on event onChange create handleSubmit function to add tasks to array embeded with useState hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const generateId = (min=0, max=10000) => {
+    return Math.floor(Math.random() * ((max - min + 1) + min));
+  };
+  const [taskText, setTaskText] = useState('');
+  const [taskStages, setTaskStages] = useState(0);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    console.log(tasks);
+  }, [tasks]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTasks(() => {
+      const task = {
+        id: generateId(),
+        text: taskText,
+        plannedStages: taskStages,
+        finishedStages: 0
+      }
+      return [...tasks, task];
+    });
+    setTaskText('');
+    setTaskStages(0);
+  };
+
   return (
     <div className="app-container">
       <div className="tasks-header">
@@ -9,28 +36,26 @@ function App() {
         <span>Controls</span>
       </div>
       <ul className="tasks-container">
-        <li className="task">
-          <span className="task-name">Task 1</span>
-          <span className="task-status">1/4</span>
-          <div className="btn-container">
-            <button>done</button>
-            <button>increase task count</button>
-            <button>delete task</button>
-          </div>
-        </li>
-        <li className="task">
-          <span className="task-name">Task 1</span>
-          <span className="task-status">1/4</span>
-          <div className="btn-container">
-            <button>done</button>
-            <button>increase task count</button>
-            <button>delete task</button>
-          </div>
-        </li>
+        {tasks &&
+            tasks.map(task => {
+              const {id, text, plannedStages, finishedStages} = task;
+              return (
+                <li key={id} className="task">
+                  <span className="task-name">{text}</span>
+                  <span className="task-status">{finishedStages} / {plannedStages}</span>
+                  <div className="btn-container">
+                    <button>done</button>
+                    <button>increase task count</button>
+                    <button>delete task</button>
+                  </div>
+                </li>
+              )
+            })        
+        }
       </ul>
-      <form className="form">
-        <input className="form-input-text" type="text" placeholder="Enter task name" />
-        <input className="form-input-number" type="number" placeholder="0"/>
+      <form className="form" onSubmit={handleSubmit}>
+        <input className="form-input-text" type="text" placeholder="Enter task name" value={taskText} onChange={e => setTaskText(e.target.value)} />
+        <input className="form-input-number" type="number" placeholder="0" value={taskStages} onChange={e => setTaskStages(e.target.value)}/>
         <button className="form-btn" type="submit">Submit</button>
       </form>
     </div>
